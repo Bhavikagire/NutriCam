6f5f2081d41dcd1404ef2abfad956d627b69ddaa
Bhavikagire <bhavikagire3035@gmail.com>
2023-09-15 01:39:40 +0530

    complete app done

---
+++ fruitInfo.js
@@ -0,0 +1,77 @@
+const fruitDatabase = {
+    "Apple": {
+        "nutritional": "Apples are a rich source of dietary fiber, vitamin C, and various antioxidants. They promote heart health, aid digestion, and may help regulate blood sugar levels.",
+        "origin": "Apples are native to Central Asia and have a long history of cultivation in regions around the world. They are one of the oldest cultivated fruits.",
+        "uses": "Apples can be enjoyed fresh as a snack, sliced in salads, used in baking pies and crisps, and turned into applesauce. They are also used in cider production."
+    },
+    "Mango": {
+        "nutritional": "Mangoes are known for being rich in vitamins A, C, and E, as well as dietary fiber. They provide antioxidants that support immune function and skin health.",
+        "origin": "Mangoes are believed to have originated in South Asia, with India being their primary center of diversity. They are now grown in tropical and subtropical regions worldwide.",
+        "uses": "Mangoes are consumed fresh, often peeled and sliced. They are used in smoothies, fruit salads, chutneys, and desserts. In some cultures, green mangoes are used in savory dishes."
+    },
+    "Orange": {
+        "nutritional": "Oranges are an excellent source of vitamin C, providing immune support and aiding iron absorption. They also contain dietary fiber, potassium, and antioxidants.",
+        "origin": "Oranges are believed to have originated in Southeast Asia. They have a rich history and have been cultivated for thousands of years in various parts of the world.",
+        "uses": "Oranges are consumed fresh, juiced for their refreshing taste, and used in various culinary applications. Orange zest and peel are used to add flavor to both sweet and savory dishes."
+    },
+    "Pomegranate": {
+        "nutritional": "Pomegranates are packed with antioxidants, including punicalagins and anthocyanins. They are a good source of vitamin C, potassium, and dietary fiber.",
+        "origin": "Pomegranates are native to the region of modern-day Iran and have been cultivated in the Mediterranean region and Middle East for centuries.",
+        "uses": "Pomegranates are enjoyed by eating the arils (seeds surrounded by juicy pulp). They are used in salads, desserts, and beverages. Pomegranate juice is also popular."
+    },
+    "Strawberry": {
+        "nutritional": "Strawberries are low in calories and high in vitamin C, manganese, and antioxidants. They support heart health, promote skin vitality, and may aid digestion.",
+        "origin": "Strawberries are native to both North America and Europe. They have a long history of cultivation and are now grown globally.",
+        "uses": "Strawberries are enjoyed fresh as a snack, in fruit salads, and as a topping for desserts like ice cream and cake. They are also used in jams, preserves, and baked goods."
+    },
+    "Avocado": {
+        "nutritional": "Avocados are rich in healthy monounsaturated fats, which are beneficial for heart health. They provide vitamins K, E, C, and B6, along with dietary fiber.",
+        "origin": "Avocados are believed to have originated in south-central Mexico, where they have been cultivated for thousands of years. They are now grown in various parts of the world.",
+        "uses": "Avocados are commonly used to make guacamole, added to salads, sandwiches, and wraps. They are also blended into smoothies or enjoyed simply by sprinkling with salt and pepper."
+    },
+    "Kiwi": {
+        "nutritional": "Kiwis are packed with vitamins C and K, dietary fiber, and antioxidants. They support immune health and may aid digestion.",
+        "origin": "Kiwis are native to China and were originally called Chinese gooseberries. They are now grown in various parts of the world.",
+        "uses": "Kiwis are often eaten as a snack, added to fruit salads, or used as a garnish. They can also be used in desserts and smoothies."
+    },
+    "Banana": {
+        "nutritional": "Bananas are a great source of potassium, vitamin C, and vitamin B6. They provide energy and are easy to digest.",
+        "origin": "Bananas are believed to have originated in Southeast Asia. They are now one of the most popular and widely cultivated fruits globally.",
+        "uses": "Bananas are commonly eaten as a quick and healthy snack. They can also be used in baking, added to cereal, or blended into smoothies."
+    },
+    "Pineapple": {
+        "nutritional": "Pineapples are rich in vitamin C, manganese, and antioxidants. They support immune health and have anti-inflammatory properties.",
+        "origin": "Pineapples are native to South America and were spread by indigenous peoples throughout Central and South America.",
+        "uses": "Pineapples are enjoyed fresh, in fruit salads, as a topping, or in beverages like piña coladas. They can also be grilled or used in cooking."
+    },
+    "Grapes": {
+        "nutritional": "Grapes are a good source of vitamins C and K, as well as antioxidants. They are known for their potential health benefits, including supporting heart health and cognitive function.",
+        "origin": "Grapes have been cultivated for thousands of years and have historical significance in various cultures.",
+        "uses": "Grapes are commonly eaten as a snack and used to make various products such as wine, juice, and raisins. They can also be added to salads and desserts."
+    },
+    "Lemon": {
+        "nutritional": "Lemons are rich in vitamin C and citric acid. They have alkalizing properties and are often used to add flavor to dishes and beverages.",
+        "origin": "Lemons are believed to have originated in Southeast Asia and have been cultivated for culinary and medicinal purposes.",
+        "uses": "Lemons are used to add flavor to both sweet and savory dishes. They are also used to make lemonade and provide a zesty aroma to various recipes."
+    },
+    "Watermelon": {
+        "nutritional": "Watermelons are hydrating and low in calories. They are a good source of hydration and provide vitamins A and C.",
+        "origin": "Watermelons have ancient origins and are believed to have originated in Africa. They have been cultivated for thousands of years.",
+        "uses": "Watermelons are often eaten fresh as a refreshing summer fruit. They can also be used in beverages, salads, and desserts."
+    },
+    "Papaya": {
+        "nutritional": "Papayas are rich in vitamins A and C, as well as enzymes like papain. They support digestion and may have anti-inflammatory effects.",
+        "origin": "Papayas are believed to have originated in Central America and have been cultivated for their tropical fruit.",
+        "uses": "Papayas are consumed fresh, often as a snack or added to fruit salads. They can also be used in smoothies, desserts, and sauces."
+    },
+    "Wall": {
+        "nutritional": "You can go with fruits I can see A wall.",
+        "origin": "NA",
+        "uses": "NA",
+    },
+    "Person": {
+        "nutritional": "You can go with fruits I can see A Person.",
+        "origin": "NA",
+        "uses": "NA",
+    }
+};
